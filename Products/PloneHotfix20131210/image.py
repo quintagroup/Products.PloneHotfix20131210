ef342b885c7717e5cc6344c3b48534a715aa62f8
Roman Kozlovskyi <krzroman@gmail.com>
2014-01-13 16:16:45 +0200

    Added info about last fix

--- Products/PloneHotfix20131210/image.py
+++ Products/PloneHotfix20131210/image.py
@@ -1,3 +1,5 @@
+# In Plone 2.0.x the imagePatch requires tag to have docstring, thus 
+# it should import and run before the docstring is removed by this patch.
 try:
     from Products.CMFPlone import imagePatch
 except:
